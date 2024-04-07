305c0cbbcaf1a1c8fd890b6622b4532882cb4307
okuraayno <ohsushi555@gmail.com>
2024-04-07 09:59:55 +0000

    top/about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+  
+  devise_for :admins, skip: [:registrations, :passwords] , controllers: {
+  sessions: "admin/sessions"
+}
+  
+  # ユーザー側
+  root 'user/homes#top'
+  get "/about" => "user/homes#about"
+  
+  scope module: :public do
+    resources :movies, only: [:index, :show] do
+      resources :reviews, only: [:show, :new, :create, :edit, :update, :destroy]
+    end
+    resources :users, only: [:index,:show,:edit,:create,:destroy,:update] 
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
