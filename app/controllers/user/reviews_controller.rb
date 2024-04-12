762f512366b07fe21f62bdb8268700a09eab7375
okuraayno <ohsushi555@gmail.com>
2024-04-12 13:25:16 +0000

    movie_model作成

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -1,52 +1,50 @@
 class User::ReviewsController < ApplicationController
 
+# set_movie呼び出し
+  before_action :set_movie, only: [:show, :new, :create, :edit, :update]
+
   def show
-    movie_id = params[:movie_id]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
-    @movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
     @review = Review.find(params[:id])
   end
 
   def new
-    movie_id = params[:movie_id]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
-    @movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
     @review = Review.new
   end
 
   def create
-    movie_id = params[:movie_id]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
-    movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
     review = Review.new(review_params)
     review.user_id = current_user.id
     if review.save
-      redirect_to movie_path(movie['id']) , notice: "You have created book successfully."
+      redirect_to movie_path(@movie['id']), notice: "You have created book successfully."
     else
       render 'new'
     end
   end
   
   def edit
-    movie_id = params[:movie_id]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
-    @movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
     @review = Review.find(params[:id])
   end
 
   def update
-    movie_id = params[:movie_id]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
-    movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
-    review = Review.find(params[:id])
-    if review.update(review_params)
-      redirect_to movie_path(movie['id']), notice: "You have updated book successfully."
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to movie_path(@movie['id']), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
+  
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to movies_path
+  end
 
   private
+
+  def set_movie
+    @movie = Movie.fetch_movie_data(params[:movie_id])
+  end
 
   def review_params
     params.require(:review).permit(:user_id, :movie_id, :title, :comment, :star)
---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+  
+# APIからのデータ取得
+  def self.fetch_movie_data(movie_id)
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API_KEY']}&language=ja"
+    JSON.parse(Net::HTTP.get(URI.parse(url)))
+  end
+
+end
