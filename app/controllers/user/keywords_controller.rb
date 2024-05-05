39c28f76c4bdcebcda506989346aa9ee63d00ead
okuraayno <ohsushi555@gmail.com>
2024-05-05 12:05:44 +0000

    [update]キーワード検索

--- app/controllers/user/keywords_controller.rb
+++ app/controllers/user/keywords_controller.rb
@@ -1,22 +1,22 @@
-class User::KeywordsController < ApplicationController  
+class User::KeywordsController < ApplicationController
 
   def search
     @keyword = params[:keyword]
     @movies = []
-
-    (1..5).each do |page|
-      url = "https://api.themoviedb.org/3/movie/now_playing?api_key=#{ENV['TMDB_API_KEY']}&language=ja&page=#{page}"
+    (1..50).each do |page|
+      url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&language=ja&page=#{page}&include_adult=false"
       response = Net::HTTP.get_response(URI.parse(url))
       if response.code == "200"
         movies = JSON.parse(response.body)
         movies['results'].each do |movie|
-          if movie['overview'].include?(@keyword)
+          if movie['overview'].include?(@keyword) # あらすじにキーワードが含まれるか判別
             @movies << movie
           end
         end
       end
     end
 
+    @movies = Kaminari.paginate_array(@movies).page(params[:page]).per(20)
     render "user/keywords/index"
   end
 
