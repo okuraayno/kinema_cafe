3703f3cce6af373ed3743afc8f44c1ee9c49c3dd
okuraayno <ohsushi555@gmail.com>
2024-05-05 13:20:18 +0000

    [add]キーワード検索フォーム追加

--- app/controllers/user/keywords_controller.rb
+++ app/controllers/user/keywords_controller.rb
@@ -3,13 +3,13 @@ class User::KeywordsController < ApplicationController
   def search
     @keyword = params[:keyword]
     @movies = []
-    (1..50).each do |page|
+    (1..100).each do |page|
       url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&language=ja&page=#{page}&include_adult=false"
       response = Net::HTTP.get_response(URI.parse(url))
       if response.code == "200"
         movies = JSON.parse(response.body)
         movies['results'].each do |movie|
-          if movie['overview'].include?(@keyword) # あらすじにキーワードが含まれるか判別
+          if movie['overview'].include?(@keyword) || movie['title'].include?(@keyword) # タイトルまたはあらすじにキーワードが含まれるか判別
             @movies << movie
           end
         end
