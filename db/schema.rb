45211bc8b296e25bdbbf736e5c7950ab9963a5b3
okuraayno <ohsushi555@gmail.com>
2024-07-10 13:56:03 +0000

    [add]newsコントローラ・モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_24_110850) do
+ActiveRecord::Schema.define(version: 2024_07_10_135317) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,12 @@ ActiveRecord::Schema.define(version: 2024_04_24_110850) do
   end
 
   create_table "movies", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
